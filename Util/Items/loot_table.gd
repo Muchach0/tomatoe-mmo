74660e294a46d0c2edadce9e299a055cba576d69
Muchach0 <guillaume.vilar@gmail.com>
2025-12-29 18:12:48 +0100

    [LOOT]: Add guaranteed and randomize drop

--- Util/Items/loot_table.gd
+++ Util/Items/loot_table.gd
@@ -3,8 +3,11 @@ class_name LootTable
 extends Resource
 
 
-## Mapped by item, weight
-@export var item_rolls : Array[ItemRoll]
+## Items that always drop
+@export var guaranteed_rolls : Array[ItemRoll]
+
+## Items that drop randomly based on weight
+@export var random_rolls : Array[ItemRoll]
 
 @export var roll_times := 1
 
@@ -13,12 +16,20 @@ var rng := RandomNumberGenerator.new()
 
 func roll_loot() -> Array[ItemStack]:
 	var items : Array[ItemStack]
-	var weights : Array = item_rolls.map(func(roll: ItemRoll): return roll.weight)
 	
-	for i in roll_times:
-		var index := rng.rand_weighted(weights)
-		var roll : ItemRoll = item_rolls[index]
+	# Add all guaranteed items
+	for roll in guaranteed_rolls:
 		if roll.item:
 			items.append(ItemStack.new(roll.item, roll.count))
+	
+	# Roll for random items
+	if random_rolls.size() > 0:
+		var weights : Array = random_rolls.map(func(roll: ItemRoll): return roll.weight)
+		
+		for i in roll_times:
+			var index := rng.rand_weighted(weights)
+			var roll : ItemRoll = random_rolls[index]
+			if roll.item:
+				items.append(ItemStack.new(roll.item, roll.count))
 	
 	return items
